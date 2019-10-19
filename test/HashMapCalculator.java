8abb386480a83a43d1692010d99c9d9cc43e816f
League Student <league@iMac-15.attlocal.net>
2019-10-19 14:57:16 -0700

    VoteProcessor, HashMapCalculator (N/A)

--- test/HashMapCalculator.java
+++ test/HashMapCalculator.java
@@ -4,10 +4,13 @@ public class HashMapCalculator {
 
 	public Object commonKeyValuePairs(HashMap<String, String> hashmap1, HashMap<String, String> hashmap2) {
 		int matchingentires = 0;
+		String checkingkey = "";
+		int checkingvalue = 0;
 		
 		for (int i = 0; i < 6; i++) {
 			for (int j = 0; j < 6; j++) {
-				if (hashmap1.containsKey(hashmap2) && hashmap1.containsValue(hashmap2)) {
+				
+				if (hashmap1.containsKey(hashmap2.get(j)) && hashmap1.containsValue(hashmap2.get(j))) {
 					matchingentires+=1;
 				}
 			}
